05dd55dc41905950e711107c9815eaea7656c2c2
Rohan Kulkarni <rohank2502@gmail.com>
2022-02-13 15:54:57 +0530

    added app

--- backend_api/app.js
+++ backend_api/app.js
@@ -1,17 +1,16 @@
-require('dotenv').config()
-const express = require('express')
+const express = require("express");
 const app = express();
-const morgan = require('morgan')
-const cookieParser = require('cookie-parser');
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(morgan('tiny'));
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+app.use(cookieParser());
 
-const user = require('./routes/user')
+const user = require("./routes/user");
 
-app.use('/api/v1',user)
+app.use("/api/v1", user);
 
 module.exports = app;
 
--- backend_api/index.js
+++ backend_api/index.js
@@ -1,10 +1,11 @@
-require('dotenv').config()
-const app = require('./app');
-const connectDB = require('./config/database');
+const dotenv = require("dotenv");
+dotenv.config();
+const app = require("./app");
+const connectDB = require("./config/database");
 
 const port = process.env.PORT;
 connectDB();
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Server running at PORT ${port}`);
-})
+});
